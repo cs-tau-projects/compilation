4158e61876e33190ddc0e6875592c9a66fe7a3f9
Maxim German <maximgerman1@nova.cs.tau.ac.il>
2025-11-26 23:07:10 +0200

    Minor fixes

--- ex2/src/Main.java
+++ ex2/src/Main.java
@@ -56,6 +56,7 @@ public class Main
 			/*************************/
 			/* [8] Close output file */
 			/*************************/
+			fileWriter.flush();
 			fileWriter.close();
 
 			/*************************************/
