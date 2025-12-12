92f7eba14aa1516eeb2dd5e15aa89f8f29ece105
Maxim <maximg@remitly.com>
2025-12-12 13:30:10 +0200

    Fixed array type assertion

--- ex3/run_tests.sh
+++ ex3/run_tests.sh
@@ -20,7 +20,7 @@ echo ""
 passed=0
 failed=0
 
-for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21; do
+for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22; do
   input_file=$(ls input/TEST_${i}_*.txt 2>/dev/null)
   expected_file=$(ls expected_output/TEST_${i}_*_Expected_Output.txt 2>/dev/null)
 
