c82ab447789e02accf6459ff3abefff8401e26ad
Maxim German <maximgerman1@mail.tau.ac.il>
2026-04-10 13:30:15 +0300

    Fixed negetive flood division results

--- ex5/src/ir/IrCommandBinopDivIntegers.java
+++ ex5/src/ir/IrCommandBinopDivIntegers.java
@@ -43,6 +43,15 @@ public class IrCommandBinopDivIntegers extends IrCommand
 	public void mipsMe(mips.MipsGenerator gen, java.util.Map<temp.Temp, String> regMap) {
 		gen.emitInstruction("div", regMap.get(t1), regMap.get(t2));
 		gen.emitInstruction("mflo", regMap.get(dst));
+		
+		String skipLabel = "Label_div_skip_" + Math.abs(this.hashCode());
+		gen.emitInstruction("mfhi", "$s0");
+		gen.emitInstruction("beq", "$s0", "$zero", skipLabel);
+		gen.emitInstruction("xor", "$s1", regMap.get(t1), regMap.get(t2));
+		gen.emitInstruction("bgez", "$s1", skipLabel);
+		gen.emitInstruction("addiu", regMap.get(dst), regMap.get(dst), "-1");
+		gen.emitLabel(skipLabel);
+		
 		gen.addSaturation(regMap.get(dst));
 	}
 }
